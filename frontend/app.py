29345348931153b18899bc23982920117780ce3b
ManiMaran <20maran21m@gmail.com>
2025-12-21 20:43:10 +0530

    Update frontend to use Render backend

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import time
 
-BACKEND_URL = "http://127.0.0.1:8000/ask"
+BACKEND_URL = "https://neuroflow-os.onrender.com/ask"
 
 ALL_AGENTS = [
     "TaskAgent",
