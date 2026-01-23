2681a76a9035045144eb3f5f34077d3e28b8d016
ManiMaran <20maran21m@gmail.com>
2026-01-23 09:40:22 +0530

    Middleware fix

--- backend/main.py
+++ backend/main.py
@@ -39,7 +39,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://neuroflow-os.vercel.app"],
+        "https://neuroflow-os.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
